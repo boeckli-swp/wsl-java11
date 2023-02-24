d1334f9d7aba1b7538643945174745f7b4a99ab4
boeckli <boeckli@swissperform.ch>
2023-02-25 00:23:34 +0100

    FIX GET VERSION

--- .github/workflows/scripts/set_mvn_version.sh
+++ .github/workflows/scripts/set_mvn_version.sh
@@ -12,11 +12,12 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 echo "### Branch is $branch"
 
 # get current version of the top level pom
-current_version=$(mvn help:evaluate -Dexpression=project.version | grep -v '\[.*')
-echo "### Current versin is: $current_version"
+MVN_VERSION=$( mvn help:evaluate -Dexpression=project.version -q -DforceStdout )
+          echo "::set-output name=version::MVN_VERSION"
+echo "### Current version is: MVN_VERSION"
 
 # extract version suffix
-suffix=$(echo $current_version | cut -d \- -f 2)
+suffix=$(echo MVN_VERSION | cut -d \- -f 2)
 echo "### Suffix is: $suffix"
 
 # build new version
